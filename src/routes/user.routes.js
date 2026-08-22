6bb5a743f983e1b9920b30b9a765999da2a98189
Camellia <thehead4ever2@gmail.com>
2026-08-22 20:47:28 +0330

    added the basic support for controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,57 +1,9 @@
 const express = require("express");
+const controller = require("../controllers/user.controller");
 const router = express.Router();
 
-const users = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-];
-
-function changeUserName(argId, argName) {
-  const user = users.find((single) => single.id == argId);
-  if (user) {
-    user.name = argName;
-    return user.id;
-  }
-  return null;
-}
-
-router.get("/", (req, res) => {
-  res.json(users);
-});
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-
-  const user = users.find((single) => single.id == id);
-
-  if (!user) {
-    res.status(404).json({ error: "not found" });
-    res.end(JSON.stringify({ error: "user not found" }));
-    return;
-  }
-  res.json(user);
-});
-router.post("/", (req, res) => {
-  const newUser = {
-    id: users.length + 1,
-    name: req.body.name,
-  };
-
-  users.push(newUser);
-
-  res.status(201).json(newUser);
-});
-router.patch("/:id", (req, res) => {
-  if (req.body.name.trim() === "") {
-    console.log(req.body.name.trim());
-    res.status(400).json({ error: "name must be a non empty string" });
-    return;
-  }
-
-  const result = changeUserName(req.params.id, req.body.name);
-  if (result) {
-    res.status(200).json(result);
-    return;
-  }
-  res.status(404).json({ error: "not found" });
-});
+router.get("/", controller.getUsers);
+router.get("/:id", controller.getUser);
+router.post("/", controller.addUser);
+router.patch("/:id", controller.changeUserName);
 module.exports = router;
